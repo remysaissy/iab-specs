104e97e90524484908fa22c50714ece95f70fd73
Rémy Saissy <remy.saissy@gmail.com>
2026-03-31 14:21:03 +0200

    feat(agentic_direct): add Organization entity

--- src/agentic_direct/v21/entities/mod.rs
+++ src/agentic_direct/v21/entities/mod.rs
@@ -1,1 +1,7 @@
 //! Agentic Direct v2.1 entities and data structures
+
+pub mod organization;
+pub use organization::{
+    Address, AddressBuilder, Contact, ContactBuilder, Organization, OrganizationBuilder,
+    OrganizationType,
+};
