80061756812c2d63ee8981604eaa0c6fbe807536
Rémy Saissy <remy.saissy@gmail.com>
2026-03-31 14:29:07 +0200

    feat(agentic_direct): add Order entity with state machine

--- src/agentic_direct/v21/entities/mod.rs
+++ src/agentic_direct/v21/entities/mod.rs
@@ -2,11 +2,18 @@
 
 pub mod account;
 pub mod creative;
+pub mod order;
+pub mod order_state_machine;
 pub mod organization;
 pub mod product;
 
 pub use account::{Account, AccountBuilder, AccountStatus};
 pub use creative::{Creative, CreativeBuilder};
+pub use order::{Order, OrderBuilder};
+pub use order_state_machine::{
+    can_transition_order, valid_order_transitions_from, OrderTransition, OrderTransitionBuilder,
+    VALID_ORDER_TRANSITIONS,
+};
 pub use organization::{
     Address, AddressBuilder, Contact, ContactBuilder, Organization, OrganizationBuilder,
     OrganizationType,
