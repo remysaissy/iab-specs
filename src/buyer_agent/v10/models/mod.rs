e1cf9dece25128d5974131151033f5b6b443b769
Rémy Saissy <remy.saissy@gmail.com>
2026-04-01 16:29:52 +0200

    feat(buyer_agent): add BookingJob and BookingRecommendation models

--- src/buyer_agent/v10/models/mod.rs
+++ src/buyer_agent/v10/models/mod.rs
@@ -1,7 +1,9 @@
+mod booking;
 mod campaign;
 mod negotiation;
 mod ucp;
 
+pub use booking::{BookingJob, BookingRecommendation};
 pub use campaign::{CampaignAllocation, CampaignBrief};
 pub use negotiation::{NegotiationOffer, NegotiationStrategy};
 pub use ucp::{AudiencePlan, UCPEmbedding};
