a85697059c53e309d8ddb42c4ea35f4e3075337e
Rémy Saissy <remy.saissy@gmail.com>
2026-04-01 16:25:16 +0200

    feat(buyer_agent): add NegotiationStrategy and NegotiationOffer models

--- src/buyer_agent/v10/models/mod.rs
+++ src/buyer_agent/v10/models/mod.rs
@@ -1,5 +1,7 @@
 mod campaign;
+mod negotiation;
 mod ucp;
 
 pub use campaign::{CampaignAllocation, CampaignBrief};
+pub use negotiation::{NegotiationOffer, NegotiationStrategy};
 pub use ucp::{AudiencePlan, UCPEmbedding};
