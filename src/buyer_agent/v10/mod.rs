a6a299d41991eb4259ed096137ab1a83fd8b8dad
Rémy Saissy <remy.saissy@gmail.com>
2026-04-01 23:27:44 +0200

    style(buyer_agent): fix rustfmt formatting for CI compliance

--- src/buyer_agent/v10/mod.rs
+++ src/buyer_agent/v10/mod.rs
@@ -180,7 +180,7 @@ mod integration_tests {
         NegotiationOffer, NegotiationStrategy, UCPEmbedding,
     };
     use crate::buyer_agent::v10::state_machines::{
-        can_transition_campaign, can_transition_deal, CampaignTransition, DealTransition,
+        CampaignTransition, DealTransition, can_transition_campaign, can_transition_deal,
     };
 
     #[test]
