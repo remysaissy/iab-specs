b150c1fb601352e9318d8626fadcf68a5965ae6b
Rémy Saissy <remy.saissy@gmail.com>
2026-04-02 14:21:43 +0200

    fix(seller_agent): register product module in models/mod.rs

--- src/seller_agent/v10/models/mod.rs
+++ src/seller_agent/v10/models/mod.rs
@@ -9,6 +9,7 @@ pub mod organization;
 pub mod package_view;
 pub mod pricing;
 pub mod pricing_rule;
+pub mod product;
 pub mod proposal;
 
 pub use change_request::ChangeRequest;
@@ -21,4 +22,5 @@ pub use organization::{SellerAccount, SellerOrganization};
 pub use package_view::{AuthenticatedPackageView, PublicPackageView};
 pub use pricing::{PricingTier, RateCard, TieredPricing};
 pub use pricing_rule::{PricingRule, VolumeDiscount};
+pub use product::{InventorySegment, SellerProduct};
 pub use proposal::{Proposal, ProposalItem, ProposalRevision};
