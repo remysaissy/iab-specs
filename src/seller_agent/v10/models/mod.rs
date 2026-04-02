2c42262e9de31df219eca7a67cb57fa4ba73ec62
Rémy Saissy <remy.saissy@gmail.com>
2026-04-02 13:41:56 +0200

    feat(seller_agent): add MediaKit and Package models

--- src/seller_agent/v10/models/mod.rs
+++ src/seller_agent/v10/models/mod.rs
@@ -2,7 +2,9 @@
 pub mod media_kit;
 pub mod negotiation;
 pub mod pricing;
+pub mod proposal;
 
 pub use media_kit::{MediaKit, Package};
 pub use negotiation::{NegotiationConfig, NegotiationRound};
 pub use pricing::{PricingTier, RateCard, TieredPricing};
+pub use proposal::{Proposal, ProposalItem, ProposalRevision};
