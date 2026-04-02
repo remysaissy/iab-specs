5f23227f44ba71a3cc29d6a433ed56be7f4cbe4d
Rémy Saissy <remy.saissy@gmail.com>
2026-04-02 13:45:30 +0200

    feat(seller_agent): add ChangeRequest model

--- src/seller_agent/v10/models/mod.rs
+++ src/seller_agent/v10/models/mod.rs
@@ -1,9 +1,15 @@
 // Seller Agent models
+pub mod change_request;
+pub mod deal_distribution;
+pub mod execution_order;
 pub mod media_kit;
 pub mod negotiation;
 pub mod pricing;
 pub mod proposal;
 
+pub use change_request::ChangeRequest;
+pub use deal_distribution::{DealDistribution, DspIntegration};
+pub use execution_order::ExecutionOrder;
 pub use media_kit::{MediaKit, Package};
 pub use negotiation::{NegotiationConfig, NegotiationRound};
 pub use pricing::{PricingTier, RateCard, TieredPricing};
