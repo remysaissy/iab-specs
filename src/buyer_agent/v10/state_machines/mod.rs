aad2c70b95ce2c7de149bd10130997809ffb2bc1
Rémy Saissy <remy.saissy@gmail.com>
2026-04-03 16:08:17 +0200

    style(buyer_agent): apply rustfmt formatting fixes

--- src/buyer_agent/v10/state_machines/mod.rs
+++ src/buyer_agent/v10/state_machines/mod.rs
@@ -2,9 +2,9 @@ mod campaign_state_machine;
 mod deal_state_machine;
 
 pub use campaign_state_machine::{
-    can_transition_campaign, valid_campaign_transitions_from, CampaignTransition,
-    VALID_CAMPAIGN_TRANSITIONS,
+    CampaignTransition, VALID_CAMPAIGN_TRANSITIONS, can_transition_campaign,
+    valid_campaign_transitions_from,
 };
 pub use deal_state_machine::{
-    can_transition_deal, valid_deal_transitions_from, DealTransition, VALID_DEAL_TRANSITIONS,
+    DealTransition, VALID_DEAL_TRANSITIONS, can_transition_deal, valid_deal_transitions_from,
 };
