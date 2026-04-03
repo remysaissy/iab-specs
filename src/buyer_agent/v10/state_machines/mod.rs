a9d20d64ecd83432113ecf5f8197f31ae6d5c1ef
Rémy Saissy <remy.saissy@gmail.com>
2026-04-03 14:47:36 +0200

    feat(buyer_agent): add LinearTVParams model

--- src/buyer_agent/v10/state_machines/mod.rs
+++ src/buyer_agent/v10/state_machines/mod.rs
@@ -2,9 +2,9 @@ mod campaign_state_machine;
 mod deal_state_machine;
 
 pub use campaign_state_machine::{
-    CampaignTransition, VALID_CAMPAIGN_TRANSITIONS, can_transition_campaign,
-    valid_campaign_transitions_from,
+    can_transition_campaign, valid_campaign_transitions_from, CampaignTransition,
+    VALID_CAMPAIGN_TRANSITIONS,
 };
 pub use deal_state_machine::{
-    DealTransition, VALID_DEAL_TRANSITIONS, can_transition_deal, valid_deal_transitions_from,
+    can_transition_deal, valid_deal_transitions_from, DealTransition, VALID_DEAL_TRANSITIONS,
 };
